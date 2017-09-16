10e4751de8f919ecf2337cdcf519418790b1625e
Danko David <dcd3001@hippocampus.pbtech>
2017-09-16 13:36:10 -0400

    trimmed bloat from setup

--- setup.py
+++ setup.py
@@ -6,22 +6,15 @@ from setuptools import setup
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
-    # TODO: put package requirements here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
+    # No requirements.
 ]
 
 setup(
     name='py_archy',
-    version='0.1.0',
+    version='1.0.0',
     description="Implementation of the popular node-archy tool in python",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + '\n',
     author="David C. Danko",
     author_email='dcdanko@gmail.com',
     url='https://github.com/dcdanko/py_archy',
@@ -36,15 +29,9 @@ setup(
     zip_safe=False,
     keywords='pyarchy',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
 )
